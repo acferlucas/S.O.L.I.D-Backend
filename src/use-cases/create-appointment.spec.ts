f9717f6d29a8d53ce1427570d7c501a139a244ec
acferlucas <lafs@discente.ifpe.edu.br>
2022-09-09 20:04:14 -0300

    finish tests

--- src/use-cases/create-appointment.spec.ts
+++ src/use-cases/create-appointment.spec.ts
@@ -22,3 +22,35 @@ describe("create an appointment", () => {
     })).resolves.toBeInstanceOf(Appointment)
   })
 });
+
+describe("create an appointment", () => {
+  it('should not be able to create an appointment with overlapping dates', async () => {
+
+    const appointmentsRepository = new InMemoryAppointmentsRepository()
+    const createAppointment = new CreateAppointment(appointmentsRepository);
+
+    const startDate = new Date()
+    const endDate = new Date()
+
+    endDate.setDate(endDate.getDate() + 5)
+
+    await createAppointment.execute({
+      customer: 'John Doe',
+      startsAt: startDate,
+      endsAt: endDate
+    })
+
+    const conflictingStartDate = new Date()
+    const conflictingEndDate = new Date()
+
+    conflictingStartDate.setDate(conflictingEndDate.getDate() + 2)
+    conflictingEndDate.setDate(conflictingEndDate.getDate() + 5)
+
+
+    expect(createAppointment.execute({
+      customer: 'John Doe',
+      startsAt: conflictingStartDate,
+      endsAt: conflictingEndDate
+    })).rejects.toBeInstanceOf(Error)
+  })
+});
