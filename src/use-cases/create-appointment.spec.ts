244a1ff4b7cc305d743c308bbc93187c2f78c514
acferlucas <lafs@discente.ifpe.edu.br>
2022-09-09 19:55:29 -0300

    create in-memory repository instace

--- src/use-cases/create-appointment.spec.ts
+++ src/use-cases/create-appointment.spec.ts
@@ -1,10 +1,13 @@
 import { describe, it, expect } from "vitest";
 import { Appointment } from "../entities/appointment";
+import { InMemoryAppointmentsRepository } from "../repositories/in-memory/in-memory-appointmentsreposositories";
 import { CreateAppointment } from "./create-appointment";
 
 describe("create an appointment", () => {
   it('should be able to create an appointment', () => {
-    const createAppointment = new CreateAppointment();
+
+    const appointmentsRepository = new InMemoryAppointmentsRepository()
+    const createAppointment = new CreateAppointment(appointmentsRepository);
 
     const startDate = new Date()
     const endDate = new Date()
--- src/use-cases/create-appointment.ts
+++ src/use-cases/create-appointment.ts
@@ -1,4 +1,5 @@
 import { Appointment } from '../entities/appointment'
+import { AppointmentsRepository } from '../repositories/appointments-repository';
 
 interface CreateAppointmentRequest {
   customer: string;
@@ -10,9 +11,22 @@ type CreateAppointmentResponse = Appointment;
 
 
 export class CreateAppointment {
+
+  constructor(
+    private appointmentsRepository: AppointmentsRepository,
+  ) { }
+
   async execute(req: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
 
+    const overlappingAppointment = await this.appointmentsRepository.findOverlappingAppointment(req.startsAt, req.endsAt)
+
+    if (overlappingAppointment) {
+      throw new Error('Overlapping appointment')
+    }
+
     const appointment = new Appointment(req)
+
+    await this.appointmentsRepository.create(appointment)
 
     return appointment
 
